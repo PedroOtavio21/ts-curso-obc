1848b64de674b67e4cfaf2ca6c723cca9565f2d8
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-08-09 12:02:14 -0300

    feat: adiciona conteudo de aula 11, sobre tipo extra enum no typescript

---
+++ 11-tipos-extras-enums/enums.ts
@@ -0,0 +1,22 @@
+// ---ENUMS---
+
+/* 
+    Tipagem exclusiva do TypeScript
+    Conjunto de constantes onde poderão ser atribuídas nomes para ajudar no decorrer do código
+
+    Basicamente, cada valor dos enuns é um valor iterável
+*/
+
+enum Roles {
+    USER,
+    ADMIN
+}
+
+enum Planets {
+    MERCURIO = 'Mercurio',
+    VENUS = 'Venus',
+    TERRA = 'Terra',
+    MARTE = 'Marte'
+}
+
+Planets.TERRA
